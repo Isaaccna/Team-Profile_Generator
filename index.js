8f0c956e5bbda238f12b7537c0acb9c978c525fb
Isaaccna <isaac.andrade1231@gmail.com>
2021-12-12 21:18:23 -0500

    generateHTML js file done

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 const generateHTML = require("./src/generateHTML");
-// team
 
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -32,6 +31,7 @@ const getManagerDetails = () => {
           name: "id",
           message: "Enter Team Manager's id:",
           validate: (id) => {
+
             if (isNaN(id) || !id) {
               return "Please enter a valid number";
             } else {
@@ -77,3 +77,116 @@ const getManagerDetails = () => {
       .catch((err) => console.log(err));
   };
     
+  const getEmployeeDetails = () => {
+    return inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "role",
+          message: "Please choose your employee's role:",
+          choices: ["Engineer", "Intern"],
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "Enter the employee's id:",
+          validate: (id) => {
+            if (isNaN(id) || !id) {
+              return "Please enter a valid number";
+            } else {
+              return true;
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "name",
+          message: "Enter the employee's name:",
+          validate: (name) => {
+            if (!name) {
+              return "Please enter employee's name!";
+            } else {
+              return true;
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "Enter the employee's email:",
+          validate: (email) => {
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+              return true;
+            } else {
+              console.log("Please enter a valid email");
+              return false;
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "github",
+          when: (input) => input.role === "Engineer",
+          message: "Enter Engineer's GitHub profile:",
+          validate: (github) => {
+            if (!github) {
+              return "Please enter a valid GitHub profile";
+            } else {
+              return true;
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "school",
+          when: (input) => input.role === "Intern",
+          message: "Enter Inter's School",
+          validate: (school) => {
+            if (!school) {
+              return "Please enter a school's name";
+            } else {
+              return true;
+            }
+          },
+        },
+        {
+          type: "confirm",
+          name: "addMoreEmployees",
+          message: "Would you like to add more team members?",
+          default: false,
+        },
+      ])
+      .then((employeeDetails) => {
+        let { id, name, email, role, github, school, addMoreEmployees } =
+          employeeDetails;
+  
+        switch (role) {
+          case "Engineer":
+            const engineer = new Engineer(id, name, email, github);
+            teamMembers.push(engineer);
+            break;
+          case "Intern":
+            const intern = new Intern(id, name, email, school);
+            teamMembers.push(intern);
+            break;
+        }
+  
+        if (addMoreEmployees) {
+          return getEmployeeDetails(teamMembers);
+        } else {
+          return teamMembers;
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  
+  getManagerDetails()
+    .then(getEmployeeDetails)
+    .then((teamProfile) => {
+      fs.writeFileSync("./dist/index.html", generateHTML(teamProfile));
+      console.log("Successfully generated team profile!");
+      //return console.log(teamProfile);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
---
+++ src/generateHTML.js
@@ -0,0 +1,107 @@
+function generateEmployeeCards(employees) {
+ 
+    const employeeCards = employees.map((employee) => {
+    switch (employee.getRole()) {
+      case "Manager":
+        return generateManagerCard(employee);
+   
+      case "Engineer":
+       return generateEngineerCard(employee);
+      
+      case "Intern":
+       return generateInternCard(employee);
+    
+      default:
+        throw new Error("Unknown role");
+    }
+  });
+  return employeeCards.join('');
+}
+
+function generateManagerCard(manager) {
+  return ` <div class="col-4 mt-4">
+    <div class="card" id="manager-card" style="width: 18rem">
+        <div class="header-style text-center">
+            <h5>${manager.name}</h5>
+            <span class="material-icons">person_outline </span>Manager
+        </div>
+        <ul class="list-group list-group-flush px-2">
+            <li class="list-group-item px-2 m-2 border">Id: ${manager.id}</li>
+            <li class="list-group-item px-2 m-2 border">
+                Email: <a href="mailto:${manager.email}">${manager.email}</a>
+            </li>
+            <li class="list-group-item px-2 m-2 border">
+                Office Number: ${manager.officeNumber}
+            </li>
+        </ul>
+    </div>
+</div>`;
+}
+
+function generateEngineerCard(engineer) {
+  return `<div class="col-4 mt-4">
+    <div class="card" id="engineer-card" style="width: 18rem">
+        <div class="header-style text-center">
+            <h5>${engineer.name}</h5>
+            <span class="material-icons">engineering</span> Engineer
+        </div>
+        <ul class="list-group list-group-flush px-2">
+            <li class="list-group-item px-2 m-2 border">Id: ${engineer.id}</li>
+            <li class="list-group-item px-2 m-2 border">
+                Email: <a href="mailto:${engineer.email}">${engineer.email}</a>
+            </li>
+            <li class="list-group-item px-2 m-2 border">
+                GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a>
+            </li>
+        </ul>
+    </div>
+</div>`;
+}
+
+function generateInternCard(intern) {
+  return `<div class="col-4 mt-4">
+    <div class="card" id="intern-card" style="width: 18rem">
+        <div class="header-style text-center">
+            <h5>${intern.getName()}</h5>
+            <span class="material-icons">school</span> Intern
+        </div>
+        <ul class="list-group list-group-flush px-2">
+            <li class="list-group-item px-2 m-2 border">Id: ${intern.id}</li>
+            <li class="list-group-item px-2 m-2 border">
+                Email: <a href="mailto:${intern.email}">${intern.email}</a>
+            </li>
+            <li class="list-group-item px-2 m-2 border">School: ${intern.school}</li>
+        </ul>
+    </div>
+</div>`;
+}
+const generateHTML = (data) => {
+    return `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="stylesheet" href="style.css" />
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+              integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+          <title>Team Profile</title>
+      </head>
+      
+      <body>
+          <nav class="nav-style">
+              <h1>Team Profile</h1>
+              <span class="material-icons"> groups </span>
+          </nav>
+          <div class="container">
+              <div class="row mt-4 mb-4 justify-content-center">
+              <!--Employee Cards-->
+              
+              </div>
+          </div>
+      </body>
+      </html>`;
+  };
+  
+  module.exports = generateHTML;
